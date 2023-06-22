9a17e51d1075df4399251bc8c74790d0fe317f2a
tkhs <tokuhausu@gmail.com>
2023-06-22 23:53:19 +0900

    [Update] Material3に対応

--- shared/src/commonMain/kotlin/ui/page/home/HomeScreen.kt
+++ shared/src/commonMain/kotlin/ui/page/home/HomeScreen.kt
@@ -1,11 +1,9 @@
 package ui.page.home
 
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
-import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.slide
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.stackAnimation
-import com.arkivanov.decompose.router.stack.pop
 import com.arkivanov.decompose.router.stack.push
 import io.github.xxfast.decompose.router.Router
 import io.github.xxfast.decompose.router.content.RoutedContent
@@ -27,9 +25,7 @@ fun HomeScreen() {
                 }
 
             Screen.Login ->
-                Button({router.pop()}) {
-                    Text("Login!!")
-                }
+                LoginScreen()
         }
     }
 }
--- shared/src/commonMain/kotlin/ui/page/login/LoginScreen.kt
+++ shared/src/commonMain/kotlin/ui/page/login/LoginScreen.kt
@@ -3,23 +3,28 @@ package ui.page.login
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen() {
     Column(modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(20.dp)){
-        TextField(value = "UserID", onValueChange = {  })
+        verticalArrangement = Arrangement.spacedBy(30.dp)){
+        TextField(value = "UserID", onValueChange = {  },colors = TextFieldDefaults.textFieldColors(containerColor = Color.White))
         TextField(value = "Password", onValueChange = {  })
-        Button(onClick = {  }) {
-            Text("Login")
+        Button(onClick = {  }, modifier = Modifier.width(280.dp)) {
+            Text("ログイン")
         }
     }
 }
