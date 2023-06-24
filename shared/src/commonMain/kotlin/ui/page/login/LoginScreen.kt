d0d70a30edc46cda71dfda6d2437528c15a25b24
tkhs <tokuhausu@gmail.com>
2023-06-24 11:12:58 +0900

    [Update] ログイン画面のテキストをMoko-resourcesのstringsから取得するように変更

--- shared/src/commonMain/kotlin/ui/page/login/LoginScreen.kt
+++ shared/src/commonMain/kotlin/ui/page/login/LoginScreen.kt
@@ -1,10 +1,14 @@
 package ui.page.login
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
@@ -24,9 +28,12 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.slide
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.stackAnimation
 import com.arkivanov.decompose.router.stack.push
+import dev.icerock.moko.resources.compose.stringResource
+import dev.tkhs.handaimanager.MR
 import io.github.xxfast.decompose.router.Router
 import io.github.xxfast.decompose.router.content.RoutedContent
 import io.github.xxfast.decompose.router.rememberRouter
@@ -90,19 +97,19 @@ fun LoginScreen() {
 fun Information(viewModel: LoginScreenViewModel,onNext:()->Unit){
     val uiState by viewModel.uiState.collectAsState()
     Column(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth()
+            .padding(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(30.dp)
     ) {
-        TextField(
-            value = uiState.userId,
-            label = { Text("説明文") },
-            onValueChange = { viewModel.onUserIdChanged(it) },
-            colors = TextFieldDefaults.textFieldColors(containerColor = Color.White),
-            singleLine = true
-        )
+        Text(stringResource(MR.strings.screen_login_attention), fontSize = 25.sp)
+        Text(stringResource(MR.strings.screen_login_tos),
+            modifier = Modifier.widthIn(0.dp,400.dp)
+                .border(width = 1.dp, color = Color.Black,shape = RoundedCornerShape(4.dp))
+                .padding(10.dp),
+            lineHeight = 25.sp)
         Button(onClick = { onNext() }, modifier = Modifier.width(280.dp),enabled = !uiState.isLoading) {
-            if(!uiState.isLoading)Text("次へ")
+            if(!uiState.isLoading)Text(stringResource(MR.strings.screen_login_accept_and_next))
             else CircularProgressIndicator(modifier =  Modifier.size(20.dp))
         }
     }
@@ -119,7 +126,7 @@ fun AuthPassword(viewModel: LoginScreenViewModel,onNext:()->Unit){
     ) {
         TextField(
             value = uiState.userId,
-            label = { Text("UserID") },
+            label = { Text(stringResource(MR.strings.ou_personal_id)) },
             isError = uiState.error.isNotEmpty(),
             onValueChange = { viewModel.onUserIdChanged(it) },
             colors = TextFieldDefaults.textFieldColors(containerColor = Color.White),
@@ -127,7 +134,7 @@ fun AuthPassword(viewModel: LoginScreenViewModel,onNext:()->Unit){
         )
         TextField(
             value = uiState.password,
-            label = {Text("Password")},
+            label = {Text(stringResource(MR.strings.ou_password))},
             isError = uiState.error.isNotEmpty(),
             supportingText = { if(uiState.error.isNotEmpty()) Text(uiState.error) else null },
             onValueChange = { viewModel.onPasswordChanged(it) },
@@ -140,7 +147,7 @@ fun AuthPassword(viewModel: LoginScreenViewModel,onNext:()->Unit){
             ),
         )
         Button(onClick = { onNext() }, modifier = Modifier.width(280.dp),enabled = !uiState.isLoading) {
-            if(!uiState.isLoading)Text("次へ")
+            if(!uiState.isLoading)Text(stringResource(MR.strings.next))
             else CircularProgressIndicator(modifier =  Modifier.size(20.dp))
         }
     }
@@ -157,7 +164,7 @@ fun AuthOtp(viewModel: LoginScreenViewModel,onNext:()->Unit){
     ) {
         TextField(
             value = uiState.otpCode,
-            label = { Text("ワンタイムパスワード") },
+            label = { Text(stringResource(MR.strings.ou_otp)) },
             isError = uiState.error.isNotEmpty(),
             supportingText = { if(uiState.error.isNotEmpty()) Text(uiState.error) else null },
             onValueChange = { viewModel.onOtpCodeChanged(it) },
@@ -169,7 +176,7 @@ fun AuthOtp(viewModel: LoginScreenViewModel,onNext:()->Unit){
             )
         )
         Button(onClick = { onNext() }, modifier = Modifier.width(280.dp),enabled = !uiState.isLoading) {
-            if(!uiState.isLoading)Text("次へ")
+            if(!uiState.isLoading)Text(stringResource(MR.strings.next))
             else CircularProgressIndicator(modifier =  Modifier.size(20.dp))
         }
     }
