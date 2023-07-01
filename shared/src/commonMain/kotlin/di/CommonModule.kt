4f7f4c668740922296d87ef62b71ad7734ccbfbe
tkhs <tokuhausu@gmail.com>
2023-07-01 15:06:44 +0900

    [Update] ホーム画面のビューモデルを追加

--- shared/src/commonMain/kotlin/di/CommonModule.kt
+++ shared/src/commonMain/kotlin/di/CommonModule.kt
@@ -3,11 +3,11 @@ package di
 import domain.repository.CleApiRepository
 import network.Idp
 import network.Cle
-import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 import ui.page.login.LoginScreenViewModel
 import domain.usecase.LoginUseCase
 import domain.repository.IdpRepository
+import ui.page.home.HomeScreenViewModel
 
 val commonModule = module{
     single{Idp()}
@@ -16,4 +16,5 @@ val commonModule = module{
     single{CleApiRepository(get())}
     single {LoginUseCase(get(),get())}
     factory { LoginScreenViewModel(get())}
+    factory { HomeScreenViewModel()}
 }
