b7a02b6635bbb9b09171ddd54317dfadc35009ba
tkhs <tokuhausu@gmail.com>
2023-09-08 16:34:34 +0900

    [Update] コードの最適化

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,6 +1,5 @@
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import org.jetbrains.compose.resources.ExperimentalResourceApi
 import ui.screen.RootScreen
 
 @Composable
