258ca0585d876e1e145c129d07d47949a229277a
tkhs <tokuhausu@gmail.com>
2023-10-05 22:16:18 +0900

    [Update] Resultクラスを使った場合も処理できるように調整
    エラーを回避するためにJvmNameアノテーションを付与

--- shared/src/commonMain/kotlin/domain/repository/ApiRepository.kt
+++ shared/src/commonMain/kotlin/domain/repository/ApiRepository.kt
@@ -4,16 +4,21 @@ import com.github.michaelbull.result.Err
 import com.github.michaelbull.result.Ok
 import com.github.michaelbull.result.Result
 import com.github.michaelbull.result.flatMap
+import com.github.michaelbull.result.mapBoth
 import com.github.michaelbull.result.mapError
 import com.github.michaelbull.result.onFailure
+import com.github.michaelbull.result.onSuccess
 import com.github.michaelbull.result.runCatching
+import com.github.michaelbull.result.toResultOr
 import data.cache.CacheManager
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.isSuccess
+import kotlinx.serialization.KSerializer
 import network.ApiError
 import util.Logger
+import kotlin.jvm.JvmName
 
 abstract class ApiRepository(private val cacheManager: CacheManager) {
     protected suspend inline fun validateHttpResponse(
@@ -49,13 +54,36 @@ abstract class ApiRepository(private val cacheManager: CacheManager) {
 
     suspend fun <T : Any> useCache(
         key:String,
+        serializer: KSerializer<T>,
         ignoreExpired:Boolean = false,
         block: suspend CacheConfig.() -> T
     ):T{
         val cacheConfig = CacheConfig()
-        return cacheManager.get<T>(key,ignoreExpired) ?: block(cacheConfig).also {
-            cacheManager.set(key,it,cacheConfig.expire)
+        return cacheManager.get<T>(key, serializer, ignoreExpired) ?: block(cacheConfig).also {
+            cacheManager.set(key, serializer, it, cacheConfig.expire)
         }
+    }
+
+    @JvmName("useCacheWithResult")
+    suspend fun <T,U> useCache(
+        key:String,
+        serializer: KSerializer<T>,
+        ignoreExpired:Boolean = false,
+        block: suspend CacheConfig.() -> Result<T,U>
+    ):Result<T,U>{
+        val cacheConfig = CacheConfig()
+        return cacheManager.get<T>(key, serializer, ignoreExpired)
+            .toResultOr {}
+            .mapBoth(
+                success = { Ok(it) },
+                failure = {
+                    block(cacheConfig).also {
+                        it.onSuccess {
+                            cacheManager.set(key, serializer, it, cacheConfig.expire)
+                        }
+                    }
+                }
+            )
     }
 
     class CacheConfig(){
