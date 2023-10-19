59eda6491e1e6fff7da06af3453a995bd94de10f
tkhs <tokuhausu@gmail.com>
2023-10-19 14:42:00 +0900

    [Update] hasKey を追加

--- shared/src/commonMain/kotlin/data/cache/CacheManager.kt
+++ shared/src/commonMain/kotlin/data/cache/CacheManager.kt
@@ -6,5 +6,6 @@ interface CacheManager {
     fun <T> get(key: String,serializer: KSerializer<T>, ignoreExpired:Boolean = false): T?
     fun <T> set(key: String, serializer: KSerializer<T>, value: T, expire: Long = 0)
     fun getAllKeys(): List<String>
+    fun hasKey(key: String): Boolean = getAllKeys().contains(key)
     fun clear()
 }
