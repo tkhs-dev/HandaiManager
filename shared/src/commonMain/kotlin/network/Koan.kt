d5af93d1bcbdca1f49e4a446e7a351a6edb84722
tkhs <tokuhausu@gmail.com>
2023-10-21 22:04:44 +0900

    [Update] スケジュール取得のAPIを追加

--- shared/src/commonMain/kotlin/network/Koan.kt
+++ shared/src/commonMain/kotlin/network/Koan.kt
@@ -22,6 +22,16 @@ interface KoanService{
     @GET("campusweb/campusportal.do?page=main&tabId=rs")
     suspend fun getRishuPage(): HttpResponse
 
+    @GET("campusweb/campusportal.do?page=main&tabId=kh")
+    suspend fun getSchedulePage(): HttpResponse
+
+    @GET("campusweb/campussquare.do?_flowId=KHW0001100-flow")
+    suspend fun getWeeklyScheduleTable(): HttpResponse
+
+    @POST("campusweb/campussquare.do")
+    @FormUrlEncoded
+    suspend fun getScheduleList(@Field("_flowExecutionKey") flowExecutionKey:String, @Field("dispType") dispType:String = "list", @Field("dispData") dispData:String = "all", @Field("_eventId_search") eventId_search:String = " 表 示 す る", @Field("startDay") startDay:String, @Field("startDay_year") startDay_year:Int, @Field("startDay_month") startDay_month:Int, @Field("startDay_day") startDay_day:Int, @Field("endDay") endDay:String, @Field("endDay_year") endDay_year: Int, @Field("endDay_month") endDay_month: Int, @Field("endDay_day") endDay_day: Int, @Field("rishuchuFlg") rishuchuFlg:Boolean, @Field("_rishuchuFlg") _rishuchuFlg:Int, @Field("kyokanCode") kyokanCode:String, @Field("shozokuCode") shozokuCode:String): HttpResponse
+
     @GET("campusweb/campussquare.do?_flowId=RSW0001000-flow")
     suspend fun getTimeTable(): HttpResponse
 
