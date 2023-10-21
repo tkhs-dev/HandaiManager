3175f8f630f910caa350e50c6f1fc3c9b3aaf53c
tkhs <tokuhausu@gmail.com>
2023-10-21 22:06:22 +0900

    [Update] デバッグコード

--- shared/src/commonMain/kotlin/ui/screen/home/page/DashboardScreenModel.kt
+++ shared/src/commonMain/kotlin/ui/screen/home/page/DashboardScreenModel.kt
@@ -6,6 +6,7 @@ import domain.repository.CleRepository
 import domain.repository.KoanRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.datetime.LocalDate
 import model.User
 
 class DashboardScreenModel(private val cleRepository: CleRepository, private val koanRepository: KoanRepository) : ScreenModel {
@@ -14,5 +15,6 @@ class DashboardScreenModel(private val cleRepository: CleRepository, private val
 
     suspend fun init(){
         _user.value = cleRepository.getUserInfo().getOr(null)
+        koanRepository.getSchedules(LocalDate.parse("2023-06-01"), LocalDate.parse("2023-06-30"))
     }
 }
